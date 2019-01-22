3a96f170f8c60e0a21ffc543711f7b6c4c4aae9c
lab303-25 <lab303-25@labeng.local>
2019-01-22 15:53:17 +0700

    enter

--- rehearsal_1.cpp
+++ rehearsal_1.cpp
@@ -1,25 +1,23 @@
 #include<iostream>
-#include<string>
 using namespace std;
 
 int main(){
     string name;
     double GPA;
-    cout<<"What is your name?: ";
-    cin>>name;
-    cout<<"What is your GPA?: ";
-    cin>>GPA;
+      cout<<"What is your name?: ";
+      cin>>name;
+      cout<<"What is your GPA?: ";
+      cin>>GPA;
+
+         if(GPA>=3.5)
+            cout<<name<<"InW Jrim Jrim!!!";
+        
+         if(GPA<3.5)
+            cout<<"Try harder,"<<name;
+        
+
+        
 
     
-    if(GPA>=3.5){
-        cout<<name<<"InW Jrim Jrim!!!";
-        
-    if(GPA<3.5){
-         cout<<"Try harder,"<<name;
-        
-
-        
-
-    }
     return 0;
 }
