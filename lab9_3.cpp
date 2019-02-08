d735f5d88f883375fc927c818f87caeaf799d6fe
lab303-25 <lab303-25@labeng.local>
2019-02-08 18:20:15 +0700

    enter

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,26 +1,38 @@
 #include<iostream>
 #include<fstream>
-#include<string>
 #include<cstdlib>
+#include<string>
 #include<cmath>
 using namespace std;
 
 int main(){
     string data;
     double datad;
-    float sum=0;
     ifstream dataIn;
     dataIn.open("score.txt");
-    int count =0;
+
+    double count =0.0;
     while (getline(dataIn,data)){
         count++;
     }
     double score[count];
+
     dataIn.close();
     dataIn.open("score.txt");
 
-    double sum=0;
-    for(int i=0;i<=count;i++){
+    double sum=0.0;
+    for(double i=0;i<=count;i++){
+        getline(dataIn,data);
+        datad=atof(data.c_str());
+        sum += datad;
+    }
+    double avg=sum/count;
+
+    dataIn.close();
+    dataIn.open("score.txt");
+
+    double sd[2]={0.0,0.0};
+    for(double i=0;i<=count;i++){
         getline(dataIn,data);
         datad=atof(data.c_str());
         sd[1] += pow(datad-avg,2);
