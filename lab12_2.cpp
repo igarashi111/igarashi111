2a9506ddc02f5e42dc03f31e4ce247f665c055df
igarashi111 <46592139+igarashi111@users.noreply.github.com>
2019-02-28 21:50:17 +0700

    Create lab12_2.cpp

---
+++ lab12_2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+const int N = 30;
+const int M = 70;
+
+void updateImage(bool [][M],int,int,int);
+
+void showImage(const bool [][M]);
+
+int main()
+{
+    bool image[N][M] = {};
+    int s,x,y;
+    do{
+        showImage(image);
+        cout << "Input your brush size and location:";
+        cin >> s >> x >> y;
+        updateImage(image,s,x,y);
+    }while(s != 0 && x != 0 && y != 0 );
+             return 0;    
+}
+
+void updateImage(bool im[][M], int s,int x,int y){
+    for(int i=0;i<N;i++){
+        for(int j=0;j<M;j++){
+            if(sqrt(pow(i-x,2) + pow(j-y,2)) <= s-1) im[i][j] = true; 
+        }
+    }
+}
+
+void showImage(const bool im[][M]){
+    for(int i =0;i<72;i++){
+        cout << "-";
+    }
+    cout << "\n";
+    for(int i=0;i<N;i++){
+        cout <<"|";
+
+        for(int j=0;j<M;j++){
+            if(im[i][j]) cout << "*";
+            else cout << " ";
+        }
+        cout << "|" << "\n";
+    }
+    for(int i=0;i<72;i++){
+        cout << "-";
+    }
+    cout << "\n";
+}
