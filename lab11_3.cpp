c6dfb22b95fdbae9e29d13309799006f723d1661
igarashi111 <46592139+igarashi111@users.noreply.github.com>
2019-02-28 21:45:59 +0700

    Create lab11_3.cpp

---
+++ lab11_3.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+void mySwap (int &,int &); //Pass variables by references
+
+int main(){
+	int x = 6, y = 9;
+	cout << "Before swapping:\n";
+	cout << "x = " << x << ", y = " << y << "\n";
+	mySwap (x,y);
+	cout << "After swapping:\n";
+	cout << "x = " << x << ", y = " << y << "\n";
+	
+	return 0;
+}
+
+void mySwap (int &x,int &y){
+	int z = x;
+	x = y;
+	y = z;
+}
