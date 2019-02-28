34770799fa9ddb6d66af72d4e1d76b9cc5176ae2
igarashi111 <46592139+igarashi111@users.noreply.github.com>
2019-02-28 21:42:33 +0700

    Create lab8_2.cpp

---
+++ lab8_2.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+void printO(int N,int M){
+	if(N > 0 && M > 0){
+		for(int a=0;a<N;a++){
+			for(int b=0;b<M;b++){
+			cout << "O";
+			}
+		cout << endl;
+		}
+	}
+	else cout << "Invalid input";
+}
+int main(){
+	
+	printO(2,2);
+	cout << "\n";
+	
+	printO(3,5);
+	cout << "\n";	
+	
+	printO(5,3);
+	cout << "\n";	
+	
+	printO(0,3);
+	cout << "\n";	
+	
+	printO(7,-1);
+	cout << "\n";	
+	
+	return 0;
+}
